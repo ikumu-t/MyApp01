30d1415c576e8ce452bd334d0a289502e2731d23
ikumu-t <ikumu.t.196@gmail.com>
2024-07-14 21:38:52 +0900

    API連携、検索機能、人気映画一覧表示実装

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,21 +1,17 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
     <div>
         <form action="{{ route('search') }}" method="GET">
             <input type="text" name="query" placeholder="Search movies...">
             <button type="submit">Search</button>
         </form>
-    </div>
-    <div>
-        <h2>Popular</h2>
+
+        <h2>Searching Results</h2>
         <ul>
             @foreach($movies as $movie)
                 <li>{{ $movie->title }}</li>
             @endforeach
         </ul>
     </div>
+
 </x-app-layout>
